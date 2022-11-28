c028c26ddd948c1363242c1a6d61db8dceed6324
Vladimir <Volor88@ya.ru>
2022-11-28 19:08:44 +0300

    added board

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./../assets/scss/App.scss";
 import { Round } from "./Round";
 import { Square } from "./Square";
+import Black_cell from "./black_cell/black_cell";
+import White_cell from "./white_cell/white_cell";
+import Board from "./board/board";
 
 const App = () => (
   <div className="app">
-    <Round />
-    <Square />
+    <Board />
   </div>
 );
 
---
+++ src/components/black_cell/black_cell.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./black_cell.css";
+
+const Black_cell = () => {
+  return <div className="black_cell"></div>;
+};
+
+export default Black_cell;
